5c610b5066b228359a727699d7a0e2a401cec867
travisbartholome <travis.bartholome@gmail.com>
2021-04-18 22:56:28 +0200

    Attempt rotation correction at beginning of landing sequence, doesn't really seem to work

--- drone/crazyflie_test.py
+++ drone/crazyflie_test.py
@@ -16,16 +16,25 @@ uri = 'radio://0/80/2M/E7E7E7E7E7'
 
 # Drone movement parameters
 HOVER_HEIGHT = 0.5
-MIN_THRUST = 10001 # Absolute min supported by crazyflie is 10001, may need to use a higher value to fly?
-MAX_THRUST = 40000 # NOTE: max supported is 60000, starting with this for safety
+# Min and max 
+MIN_THRUST = 20000 # Absolute min supported by crazyflie is 10001
+MAX_THRUST = 36000 # NOTE: max supported is 60000
+AVG_THRUST = int((MIN_THRUST + MAX_THRUST) / 2)
 DELTA_TIME = 0.02 # In seconds
-LOG_INTERVAL_MS = 100 # In milliseconds
+LOG_INTERVAL_MS = 50 # In milliseconds
 HOVER_DURATION = 5 # In seconds
 LANDING_SPEED = 0.2 # In m/s
 LANDING_DECREMENT = 0.1 # In meters
 
+# Log parameter names
+PARAM_Z_POS = 'stateEstimate.z'
+PARAM_ROLL = 'stateEstimate.roll'
+PARAM_PITCH = 'stateEstimate.pitch'
+
 # Globals
 _drone_z_position = 0.0
+_drone_roll = 0.0
+_drone_pitch = 0.0
 
 logging.basicConfig(level=logging.INFO)
 
@@ -35,15 +44,19 @@ def unlock_setpoint_commands(cf):
     cf.commander.send_setpoint(0, 0, 0, 0)
 
 # Logger for position data
-def set_drone_z_position(timestamp, log_data, log_config_name):
+def set_drone_log_vars(timestamp, log_data, log_config_name):
     global _drone_z_position
-    PARAMETER_NAME = 'stateEstimate.z'
+    global _drone_roll
+    global _drone_pitch
 
-    _drone_z_position = log_data[PARAMETER_NAME] # Update z position global variable
+    _drone_z_position = log_data[PARAM_Z_POS] # Update z position global variable
+    _drone_roll = log_data[PARAM_ROLL] # Update roll global variable
+    _drone_pitch = log_data[PARAM_PITCH] # Update pitch global variable
+    print('z: %.3f, roll: %.3f, pitch: %.3f' % (_drone_z_position, _drone_roll, _drone_pitch))
     # print(log_data)
     # print('[%d][%s]: %s = %.3f' % (timestamp, log_config_name, PARAMETER_NAME, log_data[PARAMETER_NAME]))
 
-def drone_z_pos_logging_error(log_config_name, msg):
+def drone_vars_logging_error(log_config_name, msg):
     print('Error in logging [%s]: %s', (log_config_name, msg))
 
 # Hover to start position
@@ -62,10 +75,20 @@ def run_hover_sequence(cf):
 # Immediately sends a hover setpoint to stop the drone's flight, then descends slowly
 def run_landing_sequence(cf):
     LAND_HEIGHT = 0.05 # Height in meters after which the drone will drop
+
+    # Correct rotation so the drone is (at least approximately) upright
+    # TODO: consider correcting for pitch as well as roll?
+    # while (abs(_drone_roll) > 10 or abs(_drone_pitch) > 10):
+    #     # If drone is significantly tilted, try to counteract that
+    #     cf.commander.send_setpoint(0, 0, 0, AVG_THRUST)
+    #     time.sleep(DELTA_TIME)
+
+    # Once upright, gradually descend to the ground
     while (_drone_z_position > (LAND_HEIGHT + 0.05)):
         print('in land loop')
         new_height = max(_drone_z_position - LANDING_DECREMENT, LAND_HEIGHT)
-        print(new_height) # TODO: remove
+        print(new_height)
+        # Send two hover setpoints per second (apparently necessary)
         cf.commander.send_hover_setpoint(0, 0, 0, new_height)
         time.sleep(0.5)
         cf.commander.send_hover_setpoint(0, 0, 0, new_height)
@@ -108,11 +131,13 @@ if __name__ == '__main__':
         cf = scf.cf
 
         # Set up logging config
-        log_config = LogConfig(name='Position', period_in_ms=LOG_INTERVAL_MS)
-        log_config.add_variable('stateEstimate.z', 'float')
+        log_config = LogConfig(name='StateValues', period_in_ms=LOG_INTERVAL_MS)
+        log_config.add_variable(PARAM_Z_POS, 'float')
+        log_config.add_variable(PARAM_ROLL, 'float')
+        log_config.add_variable(PARAM_PITCH, 'float')
         cf.log.add_config(log_config)
-        log_config.data_received_cb.add_callback(set_drone_z_position)
-        log_config.error_cb.add_callback(drone_z_pos_logging_error)
+        log_config.data_received_cb.add_callback(set_drone_log_vars)
+        log_config.error_cb.add_callback(drone_vars_logging_error)
         log_config.start()
 
         print("~~~~~~ Crazyflie connected ~~~~~~")
